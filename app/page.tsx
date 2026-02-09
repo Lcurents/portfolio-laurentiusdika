6a714f3c3cefaaf0aa39c49f266b5cc88e8e13b6
lcurents <laurentiusdika28@gmail.com>
2026-02-09 19:31:03 +0700

    perbaikan under development

--- app/page.tsx
+++ app/page.tsx
@@ -2,18 +2,9 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <h1>Under Development</h1>
-      </main>
+    <div className="flex flex-col min-h-screen items-center justify-center ">
+      <h1>Under Development</h1>
+      <h2>Estimated: 14 February 2026</h2>
     </div>
   );
 }
